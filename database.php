b90829953bb3f1a8da0c4b2b742b1972963263e4
Isaac <you@example.com>
2022-04-09 18:00:29 +0300

    1st commit

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+
+// Connect to MySQL
+$host_name = "localhost";
+$db_name = "shoutit";
+$user_name = "root";
+$password = "";
+
+$conn = mysqli_connect($host_name, $user_name, $password, $db_name);
+
+// Test Connection
+if (mysqli_connect_errno()){
+    echo 'Failed to connecet to MySQL: ' . mysqli_connect_error();
+}
